4eb92470e5120aee91c52582a6c817704d3f1b24
msferrer-hub <msferrer@nu-fairview.edu.ph>
2025-11-28 11:34:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const themeBtn = document.getElementById("themeBtn");
 
 themeBtn.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
-});
-document.getElementById("themeBtn").addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+
+    // Change button label depending on mode
+    if (document.body.classList.contains("dark-mode")) {
+        themeBtn.textContent = "Light Mode";
+    } else {
+        themeBtn.textContent = "Dark Mode";
+    }
 });
 
 // =============== EDIT JOB TITLE ===============

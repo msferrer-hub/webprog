3a2a42db66bad0a1f7ef4360c083718f328bdc0a
msferrer-hub <msferrer@nu-fairview.edu.ph>
2025-11-28 11:50:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 // =============== DARK MODE TOGGLE ===============
-const themeBtn = document.getElementById("themeBtn");
-
-themeBtn.addEventListener("click", function () {
+document.getElementById("themeBtn").addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
-
-    // Change button label depending on mode
-    if (document.body.classList.contains("dark-mode")) {
-        themeBtn.textContent = "Light Mode";
-    } else {
-        themeBtn.textContent = "Dark Mode";
-    }
 });
 
 
